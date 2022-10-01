48fa9e6cdbddba031b3dc01878ec3bbd09fcced7
He110te4m <i@he110.info>
2022-10-01 21:08:18 +0800

    feat: add home page

--- src/modules/style.ts
+++ src/modules/style.ts
@@ -1,10 +1,8 @@
 import type { UserModule } from '~/types'
-
 // import global style
-export const install: UserModule = async () => {
-  await Promise.all([
-    import('@unocss/reset/tailwind.css'),
-    import('../styles/main.css'),
-  ])
-  await import('uno.css')
+import '@unocss/reset/tailwind.css'
+import '../styles/main.css'
+import 'uno.css'
+
+export const install: UserModule = () => {
 }
